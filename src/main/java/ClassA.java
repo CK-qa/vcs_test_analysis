3f44260bb66c23c08819026c0c509993a5052b0a
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2019-01-22 13:24:11 +0300

    commit msg2

--- src/main/java/ClassA.java
+++ src/main/java/ClassA.java
@@ -3,4 +3,10 @@ public class ClassA {
     public String foo(String a) {
         return a;
     }
+
+    public Object bar(Object o) {
+        return null;
+    }
+
+    int a = 0.5;
 }
--- src/test/java/ClassAaTest.java
+++ src/test/java/ClassAaTest.java
@@ -1,14 +1,18 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-public class ClassATest {
+public class ClassAaTest {
 
     ClassA classA = new ClassA();
 
     @Test
     public void foo() {
         Assert.assertTrue("test msg",classA.foo("str").length() > 0);
+    }
+
+
+    @Test
+    public void bar() {
+        Assert.assertNull(classA.bar("ss"));
     }
 }
